d7adfdbdca1b6c4273cd67fa0d8767549fb1d53c
Rafael <rfl.santos02@gmail.com>
2018-10-09 16:59:52 -0300

    RsBank

---
+++ src/main/java/com/example/RsBank/RsBankApplication.java
@@ -0,0 +1,12 @@
+package com.example.RsBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsBankApplication.class, args);
+	}
+}
